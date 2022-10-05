a83c4a1a981a987e5c02d36820d0aa351c1384db
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-10-04 21:53:29 -0300

    requisito 4 concluido

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // Requisito Rota 1
 const express = require('express');
 const bodyParser = require('body-parser');
+const { String } = require('joi');
 const { talker, generateToken } = require('./utils.js/fsUtils');
 
 const app = express();
@@ -25,13 +26,27 @@ app.get('/talker/:id', async (req, res) => {
     } 
        return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   });
-// Requisito Rota 3
+// Requisito Rota 3 e 4
+const verificaEmail = (email) => String(email)
+    .toLowerCase()
+    .match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g);
 
 app.post('/login', async (req, res) => {
     const pessoas = await talker();
+    const { email, password } = req.body;
     const novaPessoa = req.body;
     pessoas.push(novaPessoa);
     const token = generateToken();
+    
+    if (!email) {
+        return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    } if (!verificaEmail(email)) {
+        return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    } if (!password) {
+        return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    } if (password) {
+ return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });    
+    }
     return res.status(200).json({ token });
 });
 module.exports = app;
