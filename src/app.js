b22f10825a1a139262e8eacfb88499d8d6b4d2c7
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-10-06 19:43:34 -0300

    requisito 8 concluido

--- src/app.js
+++ src/app.js
@@ -1,13 +1,43 @@
-// Requisito Rota 1
+// Requisito 5
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const fs = require('fs').promises;
-const { talker, generateToken, writeTalker } = require('./utils.js/fsUtils');
 
 const app = express();
 app.use(bodyParser.json());
+const path = require('path');
+const fs = require('fs').promises;
+const { talker, generateToken, writeTalker } = require('./utils.js/fsUtils');
+const { auth } = require('./middlewars.js/valitedToken');
+const { valitedName } = require('./middlewars.js/valitedName');
+const { valitedAge } = require('./middlewars.js/valitedAge');
+const { valitedTalk } = require('./middlewars.js/valitedTalk');
+const { valitedRate } = require('./middlewars.js/valitedRate');
 
+/* const { stringify } = require('querystring'); */
+
+app.post('/talker', 
+auth,
+valitedName,  
+valitedAge,
+valitedTalk,
+valitedRate,
+async (req, res) => {
+const dadosName = req.body;
+const recebebanco = await talker();
+dadosName.id = recebebanco.length + 1;
+await writeTalker(dadosName);
+return res.status(201).json(dadosName);
+});
+
+// Requisito 8
+app.get('/talker/search', auth, async (req, res) => {
+    const { q } = req.query;
+    const list = await talker();
+    const filter = list.filter((elem) => elem.name.includes(q));
+    res.status(200).json(filter);
+});
+
+// Requisito Rota 1
 const HTTP_OK_STATUS = 200;
 app.get('/', (_request, response) => {
     response.status(HTTP_OK_STATUS).send();
@@ -23,14 +53,14 @@ app.get('/talker/:id', async (req, res) => {
     const pessoas = await talker();
     const talkerPegaId = pessoas.find((param) => param.id === Number(id));
     if (talkerPegaId) {
-       return res.status(200).json(talkerPegaId);
+        return res.status(200).json(talkerPegaId);
     } 
-       return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-  });
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
 // Requisito Rota 3 e 4
 const verificaEmail = (email) => email
-    .toLowerCase()
-    .match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g);
+.toLowerCase()
+.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g);
 
 app.post('/login', async (req, res) => {
     const pessoas = await talker();
@@ -46,31 +76,9 @@ app.post('/login', async (req, res) => {
     } if (!password) {
         return res.status(400).json({ message: 'O campo "password" é obrigatório' });
     } if (password.length < 6) {
-    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });    
+      return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });    
     }
     return res.status(200).json({ token });
-});
-
-// Requisito 5
-const { auth } = require('./middlewars.js/valitedToken');
-const { valitedName } = require('./middlewars.js/valitedName');
-const { valitedAge } = require('./middlewars.js/valitedAge');
-const { valitedTalk } = require('./middlewars.js/valitedTalk');
-const { valitedRate } = require('./middlewars.js/valitedRate');
-/* const { stringify } = require('querystring'); */
-
-app.post('/talker', 
-auth,
-valitedName,
-valitedAge,
-valitedTalk,
-valitedRate,
-async (req, res) => {
-const dadosName = req.body;
-const recebebanco = await talker();
-dadosName.id = recebebanco.length + 1;
-await writeTalker(dadosName);
-return res.status(201).json(dadosName);
 });
 
 // Requisito 6
@@ -87,7 +95,16 @@ const { id } = req.params;
 const pessoas = await talker();
 const arrayPosition = await pessoas.findIndex((elem) => elem.id === Number(id));
 pessoas.splice(arrayPosition, 1);
- await fs.writeFile(path.resolve(__dirname, './talker.json'), JSON.stringify(pessoas)); 
+await fs.writeFile(path.resolve(__dirname, './talker.json'), JSON.stringify(pessoas)); 
  return res.status(204).end();
 });
+
+/* // Requisito 8
+app.get('/talker/search?q=searchTerm', valitedToken, async (req, res) => {
+    const { q } = req.query;
+    const list = await talker();
+const filter = list.findIndex((elem) => elem.name.includes(q));
+res.status(200).json(filter);
+}); */
+
 module.exports = app;
